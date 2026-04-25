3a844bd62460303a46416289ca9f9e0ee140ffea
Eyudica <e.yudica@alumno.um.edu.ar>
2026-04-25 15:35:02 -0300

    fix: remove wrong structure

--- exception/CustomerException.java
+++
@@ -1,7 +0,0 @@
-package main.java.com.bibliotech.exception;
-
-public class CustomerException extends RuntimeException{
-        public CustomerException(String message){
-            super(message);
-        }
-}
--- exception/LibraryException.java
+++
@@ -1,7 +0,0 @@
-package main.java.com.bibliotech.exception;
-
-public class LibraryException extends RuntimeException{
-    public LibraryException(String message){
-        super(message);
-    }
-}
